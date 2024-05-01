c4e230361dff5df30aa758f3619e81e2562edfaf
jolene <sitienijolene@gmail.com>
2024-05-01 18:02:22 +0300

    add an async function for fetching data from the api

--- Desktop/NucampFolder/NucampFolder/WebDev/2-Javascript/RECIPE APP/script.js
+++ Desktop/NucampFolder/NucampFolder/WebDev/2-Javascript/RECIPE APP/script.js
@@ -9,7 +9,7 @@ let searchQuery = '';
 //assigning variables to the url,api key and api id
 const APP_ID = '9c631f13';
 const APP_key = 'fb2145e4d04ff16b75f64ffee3a59e93';
-const baseURL = `https://api.edamam.com/api/recipes/v2?q=pizza&app_id=${APP_ID}&app_key=${APP_key}`;
+
 
 // adding a submit event listener to the search form
 searchForm.addEventListener('submit',(e) =>{
@@ -18,4 +18,16 @@ searchForm.addEventListener('submit',(e) =>{
     //getting search value
     searchQuery = e.target.querySelector('input').value;
     //console.log(searchQuery);
+    //calling the function that fetches the api
+    fetchAPI();
 })
+
+//async function for fetching data from api
+async function fetchAPI(){
+    const baseURL = `https://api.edamam.com/doc/open-api/recipe-search-v2.json?q=pizza&app_id=${APP_ID}&app_key=${APP_key}`;
+    //fetching the data
+    const response = await fetch(baseURL);
+    //converting the data to json
+    const recipeData = await response.json();
+    console.log(recipeData)
+}
