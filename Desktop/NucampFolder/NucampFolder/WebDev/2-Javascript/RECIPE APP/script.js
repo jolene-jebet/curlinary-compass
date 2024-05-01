75f391655f0770f0a4e2cf9c3798ff1127c482d4
jolene <sitienijolene@gmail.com>
2024-05-01 12:48:44 +0300

    grab html elements and assign them to variables for DOM manipulation

---
+++ Desktop/NucampFolder/NucampFolder/WebDev/2-Javascript/RECIPE APP/script.js
@@ -0,0 +1,7 @@
+//grabbing search form
+const searchForm = document.querySelector('form');
+//grabbing search result
+const searchResultDiv= document.querySelector('.search-result');
+//grabbing the container
+const container = document.querySelector('container');
+
